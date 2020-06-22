572719f2f2079dd98099ea99517adaad77d6031b
CharmiPatel25 <C0768448@mylambton.ca>
2020-06-22 12:41:05 -0700

    [cp] initialize variables

--- NoteApp_TeamPurple_FP/ViewController.swift
+++ NoteApp_TeamPurple_FP/ViewController.swift
@@ -9,19 +9,40 @@
 import UIKit
 import CoreData
 
-class ViewController: UIViewController
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate
 {
+   
+    var dataManager : NSManagedObjectContext!
+    var listArray = [NSManagedObject]()
+    var items: [Note] = []
+    var searchArray : [Note] = []
+    var sortedArray : [Note] = []
+    var issearch = false
 
     @IBOutlet weak var searchBar: UISearchBar!
     
     
     
-
+    @IBOutlet weak var allNotesTV: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.allNotesTV.delegate = self;
+         self.allNotesTV.dataSource = self;
+         self.searchBar.delegate = self;
+         let   appDelegate = UIApplication.shared.delegate as! AppDelegate;
+            dataManager = appDelegate.persistentContainer.viewContext;
+        
     }
 
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+           <#code#>
+       }
+       
+       func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+           <#code#>
+       }
 
-}
+}//class end
 
