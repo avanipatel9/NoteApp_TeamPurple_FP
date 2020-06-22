e3132fbc8de7ece3bc742b5c78103ccca00cbcdf
anmariyafrancis <anmariya1915@gmail.com>
2020-06-22 14:46:01 -0400

    [AF] added MapViewController swift file

---
+++ NoteApp_TeamPurple_FP/MapViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MapViewController.swift
+//  NoteApp_TeamPurple_FP
+//
+//  Created by user176498 on 6/22/20.
+//  Copyright © 2020 Avani Patel. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
