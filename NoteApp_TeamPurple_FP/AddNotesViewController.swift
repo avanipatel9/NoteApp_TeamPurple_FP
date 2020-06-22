ecdf619c355af970206249c87aa12f2dbe215fe1
CharmiPatel25 <C0768448@mylambton.ca>
2020-06-22 07:49:52 -0700

    [cp] add outlet add note screen

--- NoteApp_TeamPurple_FP/AddNotesViewController.swift
+++ NoteApp_TeamPurple_FP/AddNotesViewController.swift
@@ -7,9 +7,35 @@
 //
 
 import UIKit
+import CoreData
+import CoreLocation
 
-class AddNotesViewController: UIViewController {
+class AddNotesViewController: UIViewController,UIPickerViewDelegate, UIPickerViewDataSource,UIImagePickerControllerDelegate, UINavigationControllerDelegate, CLLocationManagerDelegate{
+    var locationManager: CLLocationManager!
+       var dataManager : NSManagedObjectContext!
+       var listArray = [NSManagedObject]()
+       var lat: Double!
+       var long: Double!
+       var textString = String()
+       var txtTitle = String()
+       var txtnoteCategory = String()
+       var categoryPickerData: [String] = [String]()
+       var noteDate = Date()
+       var imagePicker = UIImagePickerController()
+       var imageData = Data()
 
+    @IBOutlet weak var txtAdd: UITextView!
+    
+    @IBOutlet weak var edtTitle: UITextField!
+    
+    
+    @IBOutlet weak var txtCategory: UITextField!
+    
+    @IBOutlet weak var btnAddImage: UIButton!
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var addVoiceBtn: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
 
