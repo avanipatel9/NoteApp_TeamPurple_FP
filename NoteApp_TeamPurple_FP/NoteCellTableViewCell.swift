8b32eae61f96fcacdb3b7a825d9c44f9713afaea
Avani Patel <anpatel36@yahoo.in>
2020-06-24 12:22:48 -0400

    [AP] NoteCellTableViewCell file added

---
+++ NoteApp_TeamPurple_FP/NoteCellTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  NoteCellTableViewCell.swift
+//  NoteApp_TeamPurple_FP
+//
+//  Created by Avani Patel on 6/24/20.
+//  Copyright © 2020 Avani Patel. All rights reserved.
+//
+
+import UIKit
+
+class NoteCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
